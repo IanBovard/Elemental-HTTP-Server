ac4458d94b9188b85c0d7077929b6e7a6dc5cff3
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-18 10:50:46 -1000

    mvp-ish needs validation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const querystring = require('querystring');
 
-function htmlFileGen (name, symbol, number, description){
+function htmlFileGen (name, symbol, number, description, response){
   let fileName = `public/${name}.html`;
   let htmlString  = `<!DOCTYPE html>
   <html lang="en">
@@ -23,19 +23,38 @@ function htmlFileGen (name, symbol, number, description){
   fs.writeFile(fileName.toLowerCase(), htmlString, (err,data) => {
     if (err) throw err;
     process.stdout.write('great job!\n');
+    response.end();
   });
 }
 
-function indexAppend (newlink){
+function indexAppend (file, response){
   fs.readFile('./public/index.html', (err, data) => {
+    if (err) throw err;
     let indexString = data.toString();
     let indexArray = indexString.split('\n');
-    indexArray.splice(indexArray.length-3, 0, newlink);
-    fs.writeFile('./public/index.html', indexArray.join('\n'), (err, data) => {
+    indexArray.splice(indexArray.length-4, 0, file);
+    let joinedArray = indexArray.join('\n');
+    fs.writeFile('./public/index.html', joinedArray, (err, data) => {
       if (err) throw err;
       console.log('great job again!\n');
+      response.end();
     });
 
+  });
+}
+
+function getResponseGen (file, response) {
+  let uri;
+  if (file === '/'){
+    uri = 'index.html';
+  }else{
+    uri = `${file}.html`;
+  }
+  fs.readFile(`./public/${uri}`, (err,data) => {
+    if (err) throw err;
+    let parsedData = data.toString();
+    response.write(parsedData);
+    response.end();
   });
 }
 
@@ -45,23 +64,30 @@ const server = http.createServer((request, response)=> {
   request.on('data', (chunk) => {
     dataChunk.push(chunk);
   }).on('end', () => {
+
     let method = request.method;
+    let requestPath = request.url;
     let requestChunk = dataChunk.toString();
     let requestForm = querystring.parse(requestChunk);
     let elementName = requestForm.elementName;
     let elementSymbol = requestForm.elementSymbol;
     let atomicNumber = requestForm.atomicNumber;
     let elementDescription = requestForm.elementDescription;
-    let elementIndex = `    <li>
-    <a href="/${elementName.toLowerCase()}.html">${elementName}</a>
-    </li>`;
 
 
     if (method === 'POST'){
-      fs.open('./public/index.html', 'wx+', (err, fd) => {
-        htmlFileGen(elementName, elementSymbol, atomicNumber, elementDescription);
-        indexAppend(elementIndex);
+      let elementIndex = `    <li>
+      <a href='/${elementName.toLowerCase()}.html'>${elementName}</a>
+      </li>`;
+      fs.open(`./public/${elementName.toLowerCase()}.html`, 'wx+', (err, fd) => {
+        if (err) throw err;
+        htmlFileGen(elementName, elementSymbol, atomicNumber, elementDescription, response);
+        indexAppend(elementIndex, response);
       });
+    }
+
+    if (method === 'GET'){
+      getResponseGen(`${requestPath}`, response);
     }
 
   });
